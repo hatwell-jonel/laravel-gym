9ddae546d1a2a8f6c757ae82e8a9d8c8ccce8876
Jonel Hatwell <78725678+hatwell-jonel@users.noreply.github.com>
2023-04-11 23:02:27 +0800

    Authentication (#3)
    * 'commit'
    
    * added login and registration using laravel auth
    
    ---------
    
    Co-authored-by: Jonel Hatwell <>

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,6 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -65,7 +64,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'user_id' =>  'adm126',
             'password' => Hash::make($data['password']),
         ]);
     }
